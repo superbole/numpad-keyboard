e8f7e235d5e5fce5203341cc016ac94d5da71ce5
uraymeiviar <uraymeiviar@1de7c73e-3dcb-3911-0635-2725d3e644fc>
2010-10-17 21:13:31 +0000

    initial commit

---
+++ trunk/src/com/uray/numpad/NumpadView.java
@@ -0,0 +1,18 @@
+package com.uray.numpad;
+
+import android.util.AttributeSet;
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
+
+public class NumpadView extends KeyboardView 
+{
+    public NumpadView(Context context, AttributeSet attrs) 
+    {
+        super(context, attrs);
+    }
+
+    public NumpadView(Context context, AttributeSet attrs, int defStyle) 
+    {
+        super(context, attrs, defStyle);
+    }
+}
